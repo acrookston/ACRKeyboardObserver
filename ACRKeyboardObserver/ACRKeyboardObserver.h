68ad195a111028f5ce7b6574567cb1fc834f2398
Andrew Crookston <andrew@caoos.com>
2016-01-30 19:07:02 -0800

    Initial Commit

---
+++ ACRKeyboardObserver/ACRKeyboardObserver.h
@@ -0,0 +1,19 @@
+//
+//  ACRKeyboardObserver.h
+//  ACRKeyboardObserver
+//
+//  Created by Andrew C on 1/30/16.
+//  Copyright © 2016 Andrew Crookston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACRKeyboardObserver.
+FOUNDATION_EXPORT double ACRKeyboardObserverVersionNumber;
+
+//! Project version string for ACRKeyboardObserver.
+FOUNDATION_EXPORT const unsigned char ACRKeyboardObserverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACRKeyboardObserver/PublicHeader.h>
+
+
